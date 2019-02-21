cbaa70cc673ab5777dea29c0b57a060ed41d6613
Enrique Smith <enriqueesmith@gmail.com>
2019-02-21 21:43:00 +0000

    second commit

--- index.js
+++ index.js
@@ -14,16 +14,18 @@ function domainName (){
     let j=0;
     let k=0;
     
+    let temp = "";
+    
     for (let i=0;i<pronoun.length;i++)
     {
         for (let j=0;j<adj.length;j++)
         {
             for (let k=0;k<noun.length;k++)
             {
-                
+                temp += pronoun[i]+adj[j]+noun[k]+".com <br>";
             }
         }
     }
-    return (pronoun[i]+adj[j]+noun[k]+".com");
+    return temp;
 }
 
